cfcea17678c9ee9e7889afd4e72c44d6c0cdf2ed
AsadAbbas78 <assadgamerchannel1@gmail.com>
2024-04-20 12:14:36 +0500

    changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 <section className=" body-font bg-[#180142] h-[800px] w-full">
   <div className=" flex justify-center space-x-20 items-center pt-20">
     
-      <h1 className=" text-8xl font-bold text-[#7b2bbf] sm:text-4xl md:text-8xl xl:text-8xl">
+      <h1 className=" text-8xl font-bold text-[#7b2bbf] sm:text-3xl md:text-8xl xl:text-8xl">
         WEB <br />DEVELOPER
       </h1>
       
@@ -15,7 +15,7 @@ const Hero = () => {
      
     
     <div className="">
-      <div className=' h-[300px] w-[300px] bg-[#7b2bbf] rounded-full sm:h-[150px] sm:w-[150px] md:h-[300px] md:w-[300px]'></div>
+      <div className=' h-[300px] w-[300px] bg-[#7b2bbf] rounded-full sm:h-[100px] sm:w-[100px] md:h-[300px] md:w-[300px]'></div>
     </div>
   </div>
 </section>
